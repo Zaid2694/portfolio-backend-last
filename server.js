4a9b844ffe19ca3d8ecc8104bf8083221215e5e2
Zaid Saleem <zaidsaleem375@gmail.com>
2025-05-07 23:08:00 +0500

    Updated server.js to use modular route /api

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/auth", contactRoutes);
+app.use("/api", contactRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
